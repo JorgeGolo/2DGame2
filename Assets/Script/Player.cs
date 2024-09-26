ca8da4c2cce694b83d262508bb4ae1dfcfe2416e
Jorge Gómez López <jorgelogomez@gmail.com>
2024-09-26 12:52:36 +0200

    Finishing player movement and camera

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,24 +7,28 @@ public class Player : MonoBehaviour
 {
 
     private Vector3 moveDelta;
-
     private BoxCollider2D boxcollider;
+    private RaycastHit2D hit;
+
 
     private void Start()
     {
         boxcollider = GetComponent<BoxCollider2D>();
     }
 
+    private void OnCollection()
+    {
+
+    }
+
     private void FixedUpdate()
     {
-        moveDelta = Vector3.zero;
-
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
         // Reset moveDelta
-        moveDelta = new Vector3(x, y);
-
+        moveDelta = new Vector3(x,y,0);
+       
         // sprite direction, throught localScale
         if (moveDelta.x > 0)
         {
@@ -34,8 +39,36 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        // Moving
-        transform.Translate(moveDelta * Time.deltaTime);
+        // Make sure we can move in this direction by casting a box there
+        hit = Physics2D.BoxCast(
+            transform.position, 
+            boxcollider.size, 0, 
+            new Vector2(0,moveDelta.y), 
+            Math.Abs(moveDelta.y * Time.deltaTime), 
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+
+        if (hit.collider == null)
+        {
+            // Moving
+            //transform.Translate(moveDelta * Time.deltaTime);
+            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
+        }
+        hit = Physics2D.BoxCast(
+            transform.position, 
+            boxcollider.size, 0, 
+            new Vector2(moveDelta.x,0), 
+            Math.Abs(moveDelta.x * Time.deltaTime), 
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+
+        if (hit.collider == null)
+        {
+            // Moving
+            //transform.Translate(moveDelta * Time.deltaTime);
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+        
     }
 
 }
