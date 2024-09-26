6070a96e313ff9a755f77e182f7cc4390398337a
Jorge Gómez López <jorgelogomez@gmail.com>
2024-09-26 16:06:31 +0200

    Creating a level designed, and added Collidable script to interact

---
+++ Assets/Script/Collidable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// [RequireComponent(typeof(BoxCollider2D))]
+public class Collidable : MonoBehaviour
+{
+
+    public ContactFilter2D filter;
+    private BoxCollider2D boxCollider;
+    private Collider2D[] hits = new Collider2D[10];
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void Update()
+    {
+        // collision work
+        boxCollider.OverlapCollider(filter, hits);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if(hits[i] == null)
+                continue;
+
+
+            //Debug.Log(hits[i].name);
+            //if (this.tag == "NPC_0") ( ... ) // say something...
+            //if (this.tag == "modenas") ( ... ) // grant monedas...
+
+            OnCollide(hits[i]);
+
+            // clean hits
+            hits[i] = null;
+        }
+    }
+
+    protected virtual void OnCollide(Collider2D coll)
+    {
+        //Debug.Log(coll.name);
+        
+    }
+}
