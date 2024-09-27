153ab88e327380e991ab249b32b46fad2e50bf67
Jorge Gómez López <jorgelogomez@gmail.com>
2024-09-27 13:47:20 +0200

    adding script Mover, another layer of inheritance to the player to delegate movement things

---
+++ Assets/Script/Mover.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// no se puede adjuntar a nada, por eso abstracrt
+public abstract class Mover : Fighter
+{
+    
+    private Vector3 moveDelta;
+    private BoxCollider2D boxcollider;
+    private RaycastHit2D hit;
+
+    protected float ySpeed = 0.75f;
+    protected float xSpeed = 1f;
+
+    protected virtual void Start()
+    {
+        boxcollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollection()
+    {
+
+    }
+
+
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+        
+        // Reset moveDelta
+        //moveDelta = new Vector3(x,y,0);
+        //moveDelta = input;
+        moveDelta = new Vector3(input.x * xSpeed,input.y * ySpeed,0);
+       
+        // sprite direction, throught localScale
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // Make sure we can move in this direction by casting a box there
+        hit = Physics2D.BoxCast(
+            transform.position, 
+            boxcollider.size, 0, 
+            new Vector2(0,moveDelta.y), 
+            Math.Abs(moveDelta.y * Time.deltaTime), 
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+
+        if (hit.collider == null)
+        {
+            // Moving
+            //transform.Translate(moveDelta * Time.deltaTime);
+            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
+        }
+        hit = Physics2D.BoxCast(
+            transform.position, 
+            boxcollider.size, 0, 
+            new Vector2(moveDelta.x,0), 
+            Math.Abs(moveDelta.x * Time.deltaTime), 
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+
+        if (hit.collider == null)
+        {
+            // Moving
+            //transform.Translate(moveDelta * Time.deltaTime);
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+        
+    
+    }
+
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,74 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player : Fighter
+public class Player : Mover
 {
-
-    private Vector3 moveDelta;
-    private BoxCollider2D boxcollider;
-    private RaycastHit2D hit;
-
-
-    private void Start()
-    {
-        boxcollider = GetComponent<BoxCollider2D>();
-    }
-
-    private void OnCollection()
-    {
-
-    }
 
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        // Reset moveDelta
-        moveDelta = new Vector3(x,y,0);
-       
-        // sprite direction, throught localScale
-        if (moveDelta.x > 0)
-        {
-            transform.localScale = Vector3.one;
-        }
-        else if (moveDelta.x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        // Make sure we can move in this direction by casting a box there
-        hit = Physics2D.BoxCast(
-            transform.position, 
-            boxcollider.size, 0, 
-            new Vector2(0,moveDelta.y), 
-            Math.Abs(moveDelta.y * Time.deltaTime), 
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-
-        if (hit.collider == null)
-        {
-            // Moving
-            //transform.Translate(moveDelta * Time.deltaTime);
-            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
-        }
-        hit = Physics2D.BoxCast(
-            transform.position, 
-            boxcollider.size, 0, 
-            new Vector2(moveDelta.x,0), 
-            Math.Abs(moveDelta.x * Time.deltaTime), 
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-
-        if (hit.collider == null)
-        {
-            // Moving
-            //transform.Translate(moveDelta * Time.deltaTime);
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-        }
-        
+        // from Mover    
+        UpdateMotor(new Vector3(x,y,0));
+    
     }
+
+
 
 }
