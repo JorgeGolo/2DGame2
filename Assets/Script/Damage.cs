cbe0a234cb8d0d3eda2c45e7891a329bd474ef08
Jorge Gómez López <jorgelogomez@gmail.com>
2024-09-27 13:33:17 +0200

    doing Damage to Enemy, class Fighter, Damage struct

--- Assets/Script/Damage.cs
+++ Assets/Script/Damage.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damage : MonoBehaviour
+//public class Damage
+public struct Damage
 {
     public Vector3 origin;
     public int damageAmount;
-    
+
     // we are going to push the receiver
     public float pushForce;
 
---
+++ Assets/Script/Fighter.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    // public fields
+    public int hitpoint;
+    public int maxHitPoint;
+    public float pushRecoverySpeed;
+
+    // inmunity
+
+    protected float inmuneTime = 1f;
+    protected float lastInmune;
+
+    // push
+    protected Vector3 pushDirection;
+
+    // all fighters can receive damage and die
+
+    protected virtual void ReceivedDamage(Damage dmg)
+    {
+        if(Time.time - lastInmune > inmuneTime)
+        {
+            // allow to receive damage
+            lastInmune = Time.time; // now
+            hitpoint -= dmg.damageAmount;
+            pushDirection = (transform.position - dmg.origin). normalized * dmg.pushForce;
+
+            //visual damage
+            GameManager.instance.ShowText(
+                dmg.damageAmount.ToString(),
+                20,
+                Color.red,
+                transform.position,
+                Vector3.up,
+                0.5f
+            );
+
+            // death
+            if (hitpoint <= 0)
+            {
+                hitpoint = 0;
+                Death();
+            }
+        }
+    }
+    protected virtual void Death()
+    {
+
+    }
+}
